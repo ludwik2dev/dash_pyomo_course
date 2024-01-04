ce448b5e2a104dd82bbf0e075bf8471f8589bd20
ludwik2dev <140020505+ludwik2dev@users.noreply.github.com>
2024-01-04 15:28:35 +0100

    Multiple inputs and outputs

--- index.py
+++ index.py
@@ -11,23 +11,35 @@ app = Dash(__name__)
 app.layout = html.Div([
     html.Div('Hello World'),
     dcc.Dropdown(
-        id='id-dropdown',
+        id='id-dropdown-1',
         options=units,
         value=units[1],  # 'Coal 2',
         multi=False
     ),
-    html.H4(id='id-output'),
+    dcc.Dropdown(
+        id='id-dropdown-2',
+        options=units,
+        value=units[2], 
+        multi=False
+    ),
+    html.H4(id='id-output-1'),
+    html.H4(id='id-output-2'),
 ])
 
 
 @callback(
-    Output('id-output', 'children'),
-    Input('id-dropdown', 'value'),
+    Output('id-output-1', 'children'),
+    Output('id-output-2', 'children'),
+    Input('id-dropdown-1', 'value'),
+    Input('id-dropdown-2', 'value'),
     prevent_initial_call=True
 )
-def select_dropdown(value):
+def select_dropdown(value_1, value_2):
 
-    return f'User chose: {value}'
+    text_1 = f'First selection: {value_1}'
+    text_2 = f'Second selection: {value_2}'
+
+    return text_1, text_2
 
 
 if __name__ == '__main__':
