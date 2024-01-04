c639b15ab0fa3000139c50f637dcf95cd69f5e3b
ludwik2dev <140020505+ludwik2dev@users.noreply.github.com>
2024-01-04 16:32:20 +0100

    Styling bar graph

--- index.py
+++ index.py
@@ -13,6 +13,7 @@ app.layout = html.Div([
 
     dcc.Store(id='id-store-units', data=input.units),
     dcc.Store(id='id-store-results', data=input.results),
+    dcc.Store(id='id-store-colors', data=input.units_colors),
 
     html.Div('Hello World'),
     dcc.Dropdown(
@@ -47,23 +48,58 @@ def select_dropdown(value, units):
 @callback(
     Output('id-graph-results', 'figure'),
     Input('id-store-results', 'data'),
+    State('id-store-colors', 'data'),
+    State('id-store-units', 'data'),
 )
-def generate_results_graph(results):
+def generate_graph_results(results, colors, units):
+
+    sorted_units = sorted( units.items(), key=lambda x: x[1]['vc'])
+    sorted_units = dict(sorted_units).keys()
 
     fig = go.Figure()
-    for unit in results.keys():
+    for unit in sorted_units:
+        if results is None:
+            break
+        if unit not in results.keys():
+            continue
 
         x = results[unit].keys()  # each hour
         y = results[unit].values()  # each hour
+        kind = units[unit]['type']
 
         fig.add_trace(
             go.Bar(
                 x=list(x),
                 y=list(y),
                 name=unit,
+                marker=dict(color=colors[kind], opacity=0.6, line=dict(width=0.5, color='black')),
+                hovertemplate='Power: %{y} MW',
+                showlegend=False
             )
         )
-
+    fig.update_layout(
+        barmode='relative',
+        bargap=0,
+        plot_bgcolor='white',
+        height=250, 
+        margin={'r':5,'t':5,'l':5,'b':5},
+    )
+    fig.update_xaxes(
+        tickfont=dict(size=11),
+        linewidth=1,
+        linecolor='black',
+        ticks='outside',
+        mirror=True
+    )
+    fig.update_yaxes(
+        title=dict(text='Power [MW]', font=dict(size=14)),
+        tickfont=dict(size=11),
+        linewidth=1,
+        linecolor='black',
+        ticks='outside',
+        mirror=True
+    )
+    
     return fig
 
 
