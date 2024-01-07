0f3c5b8178b3cbbe2449c00d9a32a9ec2cbc040c
ludwik2dev <140020505+ludwik2dev@users.noreply.github.com>
2024-01-07 16:55:56 +0100

    Added home page

---
+++ pages/home.py
@@ -0,0 +1,12 @@
+import dash
+from dash import html
+
+
+dash.register_page(__name__, path='/')
+
+
+layout = html.Div([   
+    html.H1('Welcome home!'),
+    html.Br(),
+    html.P('Describe your app here!'),
+])
