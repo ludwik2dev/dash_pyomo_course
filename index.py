87977dfb25e84c0d36209704f0bae4e8c2170124
ludwik2dev <140020505+ludwik2dev@users.noreply.github.com>
2024-02-04 14:00:05 +0100

    Deploying Dash App

--- index.py
+++ index.py
@@ -1,6 +1,13 @@
 import dash
 from dash import Dash, html, dcc
 import dash_bootstrap_components as dbc
+from dotenv import load_dotenv
+import os
+
+
+load_dotenv()
+
+mode = os.environ.get("MODE")
 
 
 app = Dash(
@@ -25,4 +32,20 @@ app.layout = dbc.Container([
 
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    
+    if mode == 'PRD':
+
+        from waitress import serve
+        import platform
+
+
+        computer_name = platform.node()
+        wsgi_app = app.server
+
+        serve(wsgi_app, host=computer_name, port=8080)
+
+    elif mode == "DEV":
+        app.run(debug=True)
+
+    else:
+        print('Proper environment type was not provided!')
